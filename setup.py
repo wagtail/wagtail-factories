fe05f7f63eeff9fb3f3b866fc46efe0208137174
Michael van Tellingen <michael@mvantellingen.nl>
2018-09-27 20:17:25 +0200

    Bump version: 1.0.0 → 1.1.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ with open('README.rst') as fh:
 
 setup(
     name='wagtail_factories',
-    version='1.0.0',
+    version='1.1.0',
     description='Factory boy classes for wagtail',
     long_description=long_description,
     author="Michael van Tellingen",
