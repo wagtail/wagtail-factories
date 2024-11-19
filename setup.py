a6856347431eab0e2e61616b54045a75b2a7bf89
Jakub Mastalerz <jakub.mastalerz@torchbox.com>
2024-11-19 15:03:57 +0000

    drop support for Python 3.8

--- setup.py
+++ setup.py
@@ -50,7 +50,6 @@ setup(
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
